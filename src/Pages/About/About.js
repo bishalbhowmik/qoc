c0fa8bbf5f88fa0325495f86338bfbb2c6a30639
Fazlul-Karim-Shahed <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>
2024-03-17 17:12:52 +0600

    updated

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -26,12 +26,17 @@ export const About = (props) => {
                         and educators together from diverse backgrounds. Whether you are following curriculums like
                         Cambridge, EdExcel, IB, CBSE, ICSE, NCTB, or exploring our free courses, QOC provides a
                         space where learning transcends individual endeavors.
+                        QOC understands the importance of trust. With a global community of satisfied parents and a dedicated feedback section, they offer evidence of their success. Real-time feedback ensures constant progress, while collaborative projects build communication and teamwork skills – essential for future success.
+                    </p>
+
+                    <p className='mt-8'>
+                        QOC isn't just an online school; it's a vibrant learning ecosystem. It's a place where knowledge is not a solitary pursuit but a shared adventure. By joining the QOC community, your child will embark on a journey of intellectual discovery, personal growth, and global connection.
                     </p>
                 </div>
             </div>
 
             <div>
-                <div className='my-10'>
+                <div className='mb-10'>
                     <div className='text-xl text-red-800 my-5 font-bold'>Our Mission</div>
                     Our mission goes beyond conventional education; QOC is more than just a platform – it's a
                     community dedicated to the principles of collaborative learning. We aim to empower individuals
